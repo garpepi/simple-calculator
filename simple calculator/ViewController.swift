f80fd55b0538aadf4b9adcf78c7346c4e2801975
Garpepi <garpepi.aotearoa@gmail.com>
2023-04-10 08:56:11 +0700

    currencies integration, to do: - adding function for pattern add non idr

--- simple calculator/ViewController.swift
+++ simple calculator/ViewController.swift
@@ -18,7 +18,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var calculateLabel: UILabel!
     @IBOutlet weak var resultLabel: UILabel!
 
-    let currencies: currencies = .idr
+    @IBOutlet weak var errorDesc: UILabel!
+    let currencies: currencies = .usd
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -53,7 +54,7 @@ class ViewController: UIViewController {
 
     @IBAction func deleteActionField(_ sender: Any) {
         var tmpTxt = ""
-        if let txt = calculateLabel.text {
+        if let txt = calculateLabel.text?.replacingOccurrences(of: ",", with: "") {
             if txt.suffix(1) == " " {
                 tmpTxt = String(txt.dropLast(3))
             } else {
@@ -68,47 +69,55 @@ class ViewController: UIViewController {
                 resultLabel.text = ""
             } else {
                 if let result = calc(tmpTxt.components(separatedBy: " ")) {
-                    resultLabel.text = String(result)
-                    calculateLabel.text = tmpTxt
+//                    resultLabel.text = String(result)
+//                    calculateLabel.text = tmpTxt
+                    printResult(result)
                 } else {
-                    resultLabel.text = String(tmpTxt)
-                    calculateLabel.text = tmpTxt
+//                    resultLabel.text = String(tmpTxt)
+//                    calculateLabel.text = tmpTxt
+                    printResult(Double(tmpTxt) ?? 0.0)
+                    
                 }
+                printCalculate(tmpTxt)
             }
         }
     }
 
     private func assignNumber(_ number: String) {
         // if current no number, cannot press 0
-        if let txt = calculateLabel.text,
+        print("\(calculateLabel.text?.replacingOccurrences(of: ",", with: ""))  +  \(number)")
+        if let txt = calculateLabel.text?.replacingOccurrences(of: ",", with: ""),
            txt.suffix(1) == "" || txt.suffix(1) == " " || txt.suffix(2) == "/ " || txt.suffix(2) == "x ",
            number == "0" {
             // stop
             print("CANNOT 0 IN FRONT")
+            errorDesc.text = "CANNOT 0 IN FRONT"
             return
         }
 
-
-        let tmpTxt = (calculateLabel.text ?? "") + number
+        let tmpTxt = (calculateLabel.text?.replacingOccurrences(of: ",", with: "") ?? "") + number
         let countOpr = countOperator(tmpTxt)
         if countOpr > 0 {
             let arrayTxt = tmpTxt.components(separatedBy: " ")
             if let result = calc(arrayTxt),
                validateCalc(result) {
-                calculateLabel.text = tmpTxt
-                resultLabel.text = "= " + String(result)
+//                calculateLabel.text = tmpTxt
+//                resultLabel.text = "= " + String(result)
+                printCalculate(tmpTxt)
+                printResult(result)
             } else {
                 // show error or stop
                 print("NOT ELIGIBLE")
             }
         } else {
-            calculateLabel.text = (calculateLabel.text ?? "") + number
+//            calculateLabel.text = (calculateLabel.text ?? "") + number
+            printCalculate((calculateLabel.text?.replacingOccurrences(of: ",", with: "") ?? "") + number)
         }
 
     }
 
     private func assignMathOperator(_ mathOpr: String) {
-        guard let txt = calculateLabel.text else {return}
+        guard let txt = calculateLabel.text?.replacingOccurrences(of: ",", with: "") else {return}
 
         let operatorToAdd = " " + mathOpr + " "
 
@@ -126,25 +135,68 @@ class ViewController: UIViewController {
                 // calc
                 if let result = calc(arrayTmpTxt),
                    validateCalc(result) {
-                    calculateLabel.text = String(result) + operatorToAdd
-                    resultLabel.text = "= " + String(result)
+//                    calculateLabel.text = String(result) + operatorToAdd
+//                    resultLabel.text = "= " + String(result)
+                    printCalculate(String(result) + operatorToAdd)
+                    printResult(result)
                 }
             } else {
                 if let firstNumber = validateOperator(arrayTmpTxt, 0) {
-                    calculateLabel.text = String(firstNumber) + operatorToAdd
-                    resultLabel.text = "= " + String(firstNumber)
+//                    calculateLabel.text = String(firstNumber) + operatorToAdd
+//                    resultLabel.text = "= " + String(firstNumber)
+                    printCalculate(String(firstNumber) + operatorToAdd)
+                    printResult(firstNumber)
                 }
             }
         } else {
-            calculateLabel.text = txt + operatorToAdd
+//            calculateLabel.text = txt + operatorToAdd
+            printCalculate(txt + operatorToAdd)
+        }
+    }
+
+    private func appendCalculateTxt(_ txt: String, _appendTxt: String) -> String {
+        if currencies != .idr {
+            
+        }
+
+        return txt
+    }
+
+    private func printCalculate(_ txtToPrint: String) {
+        let arrayTxt = txtToPrint.components(separatedBy: " ").filter({ $0 != ""})
+        var tmpTxt = ""
+        for txt in arrayTxt {
+            if txt == "+" ||
+               txt == "-" ||
+               txt == "/" ||
+               txt == "x" {
+                tmpTxt = tmpTxt + " " + txt + " "
+            } else {
+                if currencies == .idr {
+                    tmpTxt = tmpTxt + txt.convertToCurrency()
+                } else {
+                    tmpTxt = tmpTxt + txt.convertToCurrencyWithDecimal()
+                }
+            }
+        }
+        calculateLabel.text = tmpTxt
+    }
+
+    private func printResult(_ number: Double) {
+        if currencies == .idr {
+            resultLabel.text = "= " + String(Int(number.rounded())).convertToCurrency()
+        } else {
+            resultLabel.text = "= " + String(format: "%.2f", number).convertToCurrencyWithDecimal()
         }
     }
 
     private func validateCalc(_ result: Double) -> Bool {
         if result.isLess(than: 0.0) {
+            errorDesc.text = "CANNOT LESS THAN ZERO"
             print(" CANNOT LESS THAN ZERO")
             return false
         } else if String(result).count - 1 > 12 {
+            errorDesc.text = "CANNOT MORE THAN 15 CHARACTER"
             print(" CANNOT MORE THAN 15 CHARACTER")
             return false
         } else {
